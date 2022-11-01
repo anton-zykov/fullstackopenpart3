e88d9bcceede1afc31eea29487d8380041a3ee66
Anton <anton.zykov1@gmail.com>
2022-11-01 23:57:53 +0300

    front and back deployment

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', function (req, res) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 
 
